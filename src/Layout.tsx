d9d2c0427ddea38deb2993d6625c941b6e48a189
Mathias <mathias.fossum@kartverket.no>
2025-02-27 14:46:32 +0100

    Fix heading

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,13 +1,4 @@
-import {
-  Container,
-  Flex,
-  Footer,
-  Header,
-  Heading,
-  SimpleGrid,
-  Tabs,
-  TabsTrigger,
-} from "@kvib/react"
+import { Container, Flex, Footer, Heading, Logo, Tabs, TabsList, TabsTrigger } from "@kvib/react"
 import { Outlet, useNavigate } from "react-router-dom"
 
 const tabs = [
@@ -19,25 +10,29 @@ export const Layout = () => {
   const navigate = useNavigate()
 
   return (
-    <SimpleGrid templateRows="auto 1fr auto" minH="100vh">
-      <Header showChildrenInMenu={false}>
-        <Heading>Varslinger</Heading>
-        <Tabs size="md" ml="auto">
-          {tabs.map((tab, index) => (
-            <Flex key={index}>
-              <TabsTrigger value={index.toString()} onClick={() => navigate(tab.url)}>
-                {tab.label}
-              </TabsTrigger>
-            </Flex>
-          ))}
+    <>
+      <Flex bg="gray.100" p={4} pb={5} justifyContent="space-between" alignItems="center">
+        <Logo />
+        <Heading size="3xl">Varslinger</Heading>
+        <Tabs size="lg" defaultValue="0">
+          <Flex>
+            {tabs.map((tab, index) => (
+              <TabsList key={tab.label}>
+                <TabsTrigger value={index.toString()} onClick={() => navigate(tab.url)}>
+                  {tab.label}
+                </TabsTrigger>
+              </TabsList>
+            ))}
+          </Flex>
         </Tabs>
-      </Header>
+      </Flex>
+
       <main>
         <Container maxW="1500px" p={0} py={[10, 20]} px={[10, 20, 40]}>
           <Outlet />
         </Container>
       </main>
       <Footer excludeContactInfo excludeHelp excludeNews excludeOpeningHours excludeSocialMedia />
-    </SimpleGrid>
+    </>
   )
 }
