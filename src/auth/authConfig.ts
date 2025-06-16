de2f85c6998467b64b5c635b8c27f394688a30ca
Elena Snellingen <elena.snellingen@gmail.com>
2025-06-16 12:50:51 +0200

    Hent variabler fra env i stedet for å hardkode

--- src/auth/authConfig.ts
+++ src/auth/authConfig.ts
@@ -1,11 +1,11 @@
 export const msalConfig = {
   auth: {
-    clientId: "037789ff-26e2-480c-8574-e224035c1b0b",
-    authority: "https://login.microsoftonline.com/7f74c8a2-43ce-46b2-b0e8-b6306cba73a3",
-    redirectUri: "http://localhost:3000",  
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    authority: "https://login.microsoftonline.com/" + import.meta.env.VITE_TENANT_ID,
+    redirectUri: import.meta.env.VITE_LOGIN_REDIRECT_URI,
   },
   cache: {
-    cacheLocation: "localStorage",  
+    cacheLocation: "localStorage",
     storeAuthStateInCookie: false,
   },
-};
+}
--- src/auth/msal.ts
+++ src/auth/msal.ts
@@ -1,45 +1,18 @@
-import { PopupRequest, PublicClientApplication, RedirectRequest, SsoSilentRequest } from "@azure/msal-browser";
-import { msalConfig } from "./authConfig";
-
+import {
+  PopupRequest,
+  PublicClientApplication,
+  RedirectRequest,
+  SsoSilentRequest,
+} from "@azure/msal-browser"
+import { msalConfig } from "./authConfig"
 
 export const msalInstance = new PublicClientApplication(msalConfig)
 
-export const scopes = [`${"037789ff-26e2-480c-8574-e224035c1b0b"}/.default`]
+const clientId = msalConfig.auth.clientId
 
+export const scopes = [`${clientId}/.default`]
 
-export const authenticationRequest:
-	| PopupRequest
-	| RedirectRequest
-	| SsoSilentRequest = {
-	authority: msalInstance.getConfiguration().auth.authority, 
-	scopes,
+export const authenticationRequest: PopupRequest | RedirectRequest | SsoSilentRequest = {
+  authority: msalInstance.getConfiguration().auth.authority,
+  scopes,
 }
-
-interface Config {
-    clientId: string | undefined;
-    authority: string | undefined;
-    redirect_uri: string | undefined;
-    backend_url: string | undefined;
-    regelrett_frontend_url: string | undefined;
-    regelrett_client_id: string | undefined;
-}
-
-interface Request {
-    headersOut: Record<string, string>;
-    return: (statusCode: number, body: string) => void;
-}
-
-function getConfig(r: Request): void {
-    const config: Config = {
-        clientId: process.env.VITE_CLIENT_ID,
-        authority: process.env.VITE_AUTHORITY,
-        redirect_uri: process.env.VITE_LOGIN_REDIRECT_URI,
-        backend_url: process.env.VITE_BACKEND_URL,
-        regelrett_frontend_url: process.env.VITE_REGELRETT_FRONTEND_URL,
-        regelrett_client_id: process.env.REGELRETT_CLIENT_ID,
-    };
-    r.headersOut["Content-Type"] = "application/json";
-    r.return(200, JSON.stringify(config));
-}
-
-export default { getConfig }
