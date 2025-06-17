f0aa1e1c9551fcfb58fe60ca28984400bddefd3f
Elena Snellingen <elena.snellingen@gmail.com>
2025-06-17 15:00:28 +0200

    Vis navn på innlogget bruker

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,17 @@
-import { Container, Flex, Footer, Heading, Logo, Tabs, TabsList, TabsTrigger } from "@kvib/react"
+import {
+  Container,
+  Flex,
+  Footer,
+  Heading,
+  Icon,
+  Logo,
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  Text,
+} from "@kvib/react"
 import { Outlet, useNavigate } from "react-router-dom"
+import { getAuthState } from "./auth/authState"
 
 const tabs = [
   { label: "Sending av varsel", url: "/" },
@@ -8,31 +20,38 @@ const tabs = [
 
 export const Layout = () => {
   const navigate = useNavigate()
+  const { msalInstance } = getAuthState()
+  const name = msalInstance.getAllAccounts()[0]?.name ?? "Bruker uten navn"
 
   return (
-    <>
-      <Flex bg="gray.100" p={4} pb={5} justifyContent="space-between" alignItems="center">
-        <Logo />
-        <Heading size="3xl">Varslinger</Heading>
-        <Tabs size="lg" defaultValue="0">
-          <Flex>
-            {tabs.map((tab, index) => (
-              <TabsList key={tab.label}>
-                <TabsTrigger value={index.toString()} onClick={() => navigate(tab.url)}>
-                  {tab.label}
-                </TabsTrigger>
-              </TabsList>
-            ))}
+    <Flex direction="column" minHeight="100vh">
+      <header>
+        <Flex bg="gray.100" p={8} justifyContent="space-between" alignItems="center">
+          <Logo />
+          <Heading size="3xl">Varslingstjeneste for egenregistrering</Heading>
+          <Flex alignItems="center" justifyContent="space-between">
+            <Icon icon="person" />
+            <Text>{name}</Text>
+            <Tabs ml={8} size="lg" defaultValue="0">
+              <Flex>
+                {tabs.map((tab, index) => (
+                  <TabsList key={tab.label}>
+                    <TabsTrigger value={index.toString()} onClick={() => navigate(tab.url)}>
+                      {tab.label}
+                    </TabsTrigger>
+                  </TabsList>
+                ))}
+              </Flex>
+            </Tabs>
           </Flex>
-        </Tabs>
-      </Flex>
-
-      <main>
+        </Flex>
+      </header>
+      <main style={{ flex: 1 }}>
         <Container maxW="1500px" p={0} py={[10, 20]} px={[10, 20, 40]}>
           <Outlet />
         </Container>
       </main>
       <Footer excludeContactInfo excludeHelp excludeNews excludeOpeningHours excludeSocialMedia />
-    </>
+    </Flex>
   )
 }
