d1e2584798ee68850d829c8a41ac4fc6cef2e86e
Christian H. Mosveen <christian.mosveen@kartverket.no>
2025-08-13 14:57:02 +0200

    Legg til rette for å kunne kjøre opp i flere miljøer (#176)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,18 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    watch: {
+      usePolling: true,
+    },
+    host: true, // trengs for at Docker Container port mapping skal fungere
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:8081",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
   },
-});
+})
