ef324dfe5f4c7a63e65a433237e039cda3651192
Elena Snellingen <elena.snellingen@gmail.com>
2025-08-25 12:21:42 +0200

    Legg på tekster for epost og fiks bug på valg av sms tekst førstegangsvarslign

--- src/utils/tekster.ts
+++ src/utils/tekster.ts
@@ -2,3 +2,26 @@ export const førstegangsvarsling_sms =
   "Ny tjeneste fra Kartverket: Nå kan du selv registrere utvalgte opplysninger om din eiendom. Gå til våre nettsider og logg inn på Eiendomsregisteret. Mvh Kartverket"
 export const revarsling_sms =
   "Husk at du nå kan selv registrere utvalgte opplysninger om din eiendom. Gå til våre nettsider og logg inn på Eiendomsregisteret. Mvh Kartverket"
+
+export const førstegangsvarsling_epost_emnefelt =
+  "Registrer opplysninger om din eiendom i Eiendomsregisteret hos Kartverket"
+
+export const førstegangsvarsling_epost_innhold = `Hei,
+
+Nå kan du selv registrere utvalgte opplysninger om egen eiendom i en ny tjeneste fra Kartverket.
+
+Du kan logge inn i Eiendomsregisteret på Kartverkets nettsider for å se og registrere informasjonen.
+
+Vennlig hilsen
+Kartverket`
+
+export const revarsling_epost_emnefelt = "Påminnelse – oppdater opplysninger om din eiendom"
+
+export const revarsling_epost_innhold = `Hei,
+
+Vi minner om at du kan logge inn i Eiendomsregisteret på Kartverkets nettsider for å registrere utvalgte opplysninger om din eiendom.
+
+Det tar bare noen minutter, og du bidrar til at eiendomsinformasjonen blir oppdatert og korrekt.
+
+Vennlig hilsen
+Kartverket`
