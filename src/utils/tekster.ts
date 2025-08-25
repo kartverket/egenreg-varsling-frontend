7702e39edb7ac383cd4cbacfb7fad9ad7765dbd9
Elena Snellingen <elena.snellingen@gmail.com>
2025-08-25 13:00:53 +0200

    Legg på spacing i tekstmelding

--- src/utils/tekster.ts
+++ src/utils/tekster.ts
@@ -1,7 +1,13 @@
-export const førstegangsvarsling_sms =
-  "Ny tjeneste fra Kartverket: Nå kan du selv registrere utvalgte opplysninger om din eiendom. Gå til våre nettsider og logg inn på Eiendomsregisteret. Mvh Kartverket"
-export const revarsling_sms =
-  "Husk at du nå kan selv registrere utvalgte opplysninger om din eiendom. Gå til våre nettsider og logg inn på Eiendomsregisteret. Mvh Kartverket"
+export const førstegangsvarsling_sms = `Ny tjeneste fra Kartverket: Nå kan du selv registrere utvalgte opplysninger om din eiendom. 
+
+Gå til våre nettsider og logg inn på Eiendomsregisteret. 
+
+Mvh Kartverket`
+export const revarsling_sms = `Husk at du nå kan selv registrere utvalgte opplysninger om din eiendom. 
+  
+Gå til våre nettsider og logg inn på Eiendomsregisteret. 
+  
+Mvh Kartverket`
 
 export const førstegangsvarsling_epost_emnefelt =
   "Registrer opplysninger om din eiendom i Eiendomsregisteret hos Kartverket"
