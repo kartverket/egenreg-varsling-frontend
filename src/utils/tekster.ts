b56f800e9cec22b76060b46df050933f985ef19f
Elena Snellingen <elena.snellingen@gmail.com>
2025-09-02 14:58:30 +0200

    Test med bold

--- src/utils/tekster.ts
+++ src/utils/tekster.ts
@@ -7,15 +7,24 @@ Logg inn på Eiendomsregisteret på våre nettsider.
 Mvh Kartverket`
 
 export const førstegangsvarsling_epost_emnefelt =
-  "Registrer opplysninger om din eiendom i Eiendomsregisteret hos Kartverket"
+  "Egenregistrer opplysninger om din eiendom i Eiendomsregisteret hos Kartverket"
 
 export const førstegangsvarsling_epost_innhold = `Hei,
 
-Nå kan du selv registrere utvalgte opplysninger om egen eiendom i en ny tjeneste fra Kartverket.
+Nå kan du selv registrere opplysninger om din eiendom i en ny tjeneste fra Kartverket.
 
-Du kan logge inn i Eiendomsregisteret på Kartverkets nettsider for å se og registrere informasjonen.
+Logg inn på Eiendomsregisteret på våre nettsider for å se og registrere informasjonen.
 
-Vennlig hilsen
+<b>Hva er egenregistrering?</b>
+
+Informasjon fra Eiendomsregisteret brukes av privatpersoner, næringsliv og offentlige myndigheter i en rekke tjenester vi som samfunn er helt avhengig av. Derfor er det viktig at registeret er så oppdatert som mulig. 
+
+Egenregistrering er en del av en prøveordning hvor eiere og festere med eiendom i utvalgte kommuner selv kan registrere enkelte opplysninger. Du kan når som helst endre eller slette opplysningene du registrerer.
+
+Les mer på kartverket.no.
+
+
+Med vennlig hilsen
 Kartverket`
 
 export const revarsling_epost_emnefelt = "Påminnelse – registrer opplysninger om din eiendom"
