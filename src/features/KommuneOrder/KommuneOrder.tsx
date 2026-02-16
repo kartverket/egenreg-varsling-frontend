813412ecd9bfaf856e8cff20b7488f2e3d1bc364
Mathias Fossum <mathias.fossum@kartverket.no>
2026-02-16 11:38:54 +0000

    Vi tydlig hvilket miljø du er i.

--- src/features/KommuneOrder/KommuneOrder.tsx
+++ src/features/KommuneOrder/KommuneOrder.tsx
@@ -1,9 +1,25 @@
-import { Box } from "@kvib/react"
+import { Box, Heading } from "@kvib/react"
 import CreateKommuneOrder from "./CreateKommuneOrder"
 
 const KommuneOrder = () => {
+  const env = (): "dev" | "prod" | "localhost" | "unknown" => {
+    const env = window.location.hostname
+
+    if (env.includes("atgcp1-dev")) {
+      return "dev"
+    }
+    if (env.includes("atgcp1-prod")) {
+      return "prod"
+    }
+
+    if (env.includes("localhost")) {
+      return "localhost"
+    } else return "unknown"
+  }
+
   return (
     <Box>
+      <Heading>Du er nå i {env()}</Heading>
       <CreateKommuneOrder />
     </Box>
   )
